89b9f0d1fd58ebd732ff836fe67dcfba24de03d3
fridacetti <frjo0003@stud.kea.dk>
2026-05-24 18:35:34 +0200

    Procesydelse - done

--- src/components/ProcesYdelse.tsx
+++ src/components/ProcesYdelse.tsx
@@ -2,11 +2,11 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 export default function ProcesYdelse({ blok }: { blok: any }) {
   return (
-    <section className="w-full" {...storyblokEditable(blok)}>
-      <div className="flex flex-col gap-6 md:grid md:grid-cols-2">
-        <h2>{blok.subtitle}</h2>
-        <p>{blok.text}</p>
-        <div className="grid grid-cols-2 gap-6">
+    <section className="md:px-25" {...storyblokEditable(blok)}>
+      <div className="flex flex-col gap-6">
+        <h2 className="text-[#282828] text-2xl font-bold md:text-4xl mx-10">{blok.subtitle}</h2>
+        <p className="text-[#282828] font-normal max-w-3xl mx-10">{blok.text}</p>
+        <div className="grid lg:grid-cols-3 gap-10">
           {blok.steps?.map((step: any) => (
             <StoryblokComponent key={step._uid} blok={step} />
           ))}
--- src/components/ProcesYdelseCard.tsx
+++ src/components/ProcesYdelseCard.tsx
@@ -2,10 +2,14 @@ import { storyblokEditable } from "@storyblok/react";
 
 export default function ProcesYdelseCard({ blok }: { blok: any }) {
   return (
-    <div {...storyblokEditable(blok)}>
-      <h1>{blok.number}</h1>
-      <h2>{blok.subtitle}</h2>
-      <p>{blok.text}</p>
+    <div className="bg-[#dcded1] rounded-md shadow-xl p-10 mx-10 md:p-6" {...storyblokEditable(blok)}>
+      <div className="flex flex-row gap-4">
+        <h1 className="text-6xl font-bold text-[#50543B]">{blok.number}</h1>
+        <div>
+          <h3 className="text-[#50543B] text-2xl font-semibold">{blok.subtitle}</h3>
+          <p className="text-[#282828] font-normal text-sm">{blok.text}</p>
+        </div>
+      </div>
     </div>
   );
 }
