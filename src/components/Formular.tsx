87250044ae4d654e2e708054a8a0739223038156
Alberte-Remmer <alre0003@stud.kea.dk>
2026-05-26 13:03:44 +0200

    feat: enhance Form component with image upload functionality and form validation

--- src/components/Formular.tsx
+++ src/components/Formular.tsx
@@ -3,8 +3,8 @@ import Form from "./Form";
 
 export default function Formular() {
   return (
-    <div className="bg-[#545943]">
-      <h2>Send os en besked</h2>
+    <div className="bg-[#545943] text-white px-8 py-16 md:px-20 lg:px-36">
+      <h2 className="mb-4">Send os en besked</h2>
       <Form />
     </div>
   );
